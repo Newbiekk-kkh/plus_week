b1b31920e0f45d28c7c9ae60bec4f64c5a78660e
Newbiekk <newbiekk1126@gmail.com>
2024-12-17 19:27:44 +0900

    refactor : 재사용성이 높은 findById 메서드들을 default 메서드로 선언 후 이에 맞게 리팩토링

--- src/main/java/com/example/demo/repository/ReservationRepository.java
+++ src/main/java/com/example/demo/repository/ReservationRepository.java
@@ -4,7 +4,9 @@ import com.example.demo.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -32,4 +34,8 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
             @Param("startAt") LocalDateTime startAt,
             @Param("endAt") LocalDateTime endAt
     );
+
+    default Reservation findByIdOrElseThrow(Long reservationId) {
+        return findById(reservationId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 ID의 예약을 찾을 수 없습니다.") );
+    }
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -11,4 +13,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
     List<User> findByIdIn(List<Long> userIds);
+
+    default User findByIdOrElseThrow(Long userId) {
+        return findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 ID의 유저를 찾을 수 없습니다."));
+    }
 }
