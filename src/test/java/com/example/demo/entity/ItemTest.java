b5f1946e7e6aa5bca889f9be918fd230e6f8b39c
Newbiekk <newbiekk1126@gmail.com>
2024-12-19 02:37:37 +0900

    test : Item Entity 단위 테스트 추가
    status의 nullable = false 가 동작하는지 확인하는 테스트 코드 추가

---
+++ src/test/java/com/example/demo/entity/ItemTest.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+    @Mock
+    private User manager;
+
+    @Mock
+    private User owner;
+
+    @Test
+    @DisplayName("status 의 nullable = false 가 제대로 동작하는지 확인")
+    void StatusNotNullable() {
+        // given
+        Item item = new Item("야구방망이", "단단함", manager, owner);
+
+        // when, then
+        assertTrue(item.getStatus() == null);
+    }
+
+}
